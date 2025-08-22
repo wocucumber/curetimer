06077a4f72e0eb860e9e033684b80c6ebfd99372
wocucumber <wocucumber@gmail.com>
2025-08-22 16:25:49 +0900

    fix button position

--- js/pageManager.js
+++ js/pageManager.js
@@ -22,6 +22,9 @@ class Page {
       setTimeout(() => {
         $(".page-shown").removeClass("page-shown");
         this.#jq.fadeIn(500).addClass("page-shown");
+        setTimeout(() => {
+          this.#jq.display = null;
+        }, 500);
       }, 500);
     } else {
       $(".page-shown").removeClass("page-shown");
